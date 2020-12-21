b618f9254be6c2574addf294c32fdad088757f9e
Nak Nik <67131798+NikNak-youtube@users.noreply.github.com>
2020-12-20 17:34:47 -0800

    Create other popup javascript.js

---
+++ other popup javascript.js
@@ -0,0 +1,4 @@
+SameSite="None; Secure" src="https://static.landbot.io/landbot-3/landbot-3.0.0.js"
+var myLandbot = new Landbot.Popup({
+    configUrl: 'https://chats.landbot.io/v3/H-782473-OZ42GHZNW7WKHRWS/index.json',
+  });
